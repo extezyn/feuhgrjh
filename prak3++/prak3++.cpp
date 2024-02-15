997af02c618e1b8a112d54e2ca702ec4487d22cb
extezyn <romafalko2006@gmail.com>
2024-02-15 11:11:57 +0300

    zxc

---
+++ prak3++/prak3++.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	int arr[3][5];
+	double sum = 0;
+	for (int i = 0; i < 3; i++) {
+		for (int j=0; j<5; j++)
+		std::cin >> arr[i][j];
+	}
+
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 5; j++) {
+			sum += arr[i][j];
+		}
+		std::cout << sum/5<<" ";
+		sum = 0;
+	}
+}
+
